58897be83bbc2447d86e5511c58d9e81ce3795f7
Narsis <narsiskh.2013@gmail.com>
2023-09-15 00:57:51 +0330

    Improve and completing JavaScript.

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,29 @@
-// Select all numbers, operators, plus, all clear and equal buttons
-const buttons = document.querySelectorAll('.number, .key--operator, .key--plus');
-const acButtons = document.querySelector('.all_clear');
-const equalButtons = document.querySelector('.equal')
+const calculator_input = document.querySelector("input.calculating");
+const keys = document.querySelectorAll('.key');
+const ac_button = document.querySelector('.all_clear');
+const eq_button = document.querySelector('.equal');
+const delete_button = document.querySelector('.delete')
 
-// Add click event listener to each button
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        const value = button.textContent;
-        const input = document.querySelector('.calculating');
+
+ac_button.addEventListener('click', () =>
+calculator_input.value = '')
+
+delete_button.addEventListener('click', () => {
+    const inputValue = calculator_input.value;
+    calculator_input.value = inputValue.slice(0, inputValue.length-1);
+})
+
+for (const btn of keys) {
+    btn.addEventListener('click', () => {
+        const btnValue = btn.innerHTML
+        const inputValue = calculator_input.value;
+        const operator = ["+", "-", "x", "÷", "%", "."]
+
+        if(!(operator.includes(btnValue) && operator.includes(inputValue[inputValue.length-1])))
+            calculator_input.value += btnValue
+    })
+}
+
+eq_button.addEventListener('click', () => {
+    calculator_input.value = eval(calculator_input.value.replace("x", "*").replace("÷", "/"))
+})
